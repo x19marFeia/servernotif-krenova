ab01fbfbc45b89f64b72b9a92fd816699d884ef1
krenovaleo <jerygaming124@gmail.com>
2024-05-23 10:18:12 +0700

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require("express")
 var admin = require("firebase-admin")
 var cors = require("cors");
 var serviceAccount = require("./safetystove-3ff9e-firebase-adminsdk-kwn5x-3cb2b7dc09.json");
+// const { getFirestore, Timestamp, FieldValue, Filter, initializeApp } = require('firebase-admin/firestore');
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 const firebaseConfig = {
@@ -16,7 +17,9 @@ const firebaseConfig = {
   appId: "1:11558714996:web:38e2063d31e8a7878cdb21",
   measurementId: "G-N4YTR54HPY"
 };
+
 admin.initializeApp(firebaseConfig);
+const db = admin.firestore()
 
 
 
@@ -56,82 +59,73 @@ app.post("/send", async function (req, res) {
     token: req.body.token,
   }
   await notifAvailable( messageNotRunning)
-  // admin.database().ref('stove').on('value', (sn) => {
-  //   sn.forEach(async (st) => {
-  //     const data = st.val()
-  //     res.send({
-  //       title: `${data.fcmToken}`
-  //     });
-  //     notifConditioning(data.notifCondition, data.timeOff, data.fcmToken)
-  //   })
-  // })
+
 
 });
 
-function toStringFromInt(inp) {
-  switch(inp){
-    case 1:
-      return "1 minutes"
-      // break;
-      case 5:
-        return "5 minutes"
-        // break;
-        case 10:
-          return "10 minutes"
-          // break;
-          case 60:
-            return "1 hours"
-            // break;
-            default:
-              return "1 min"
-  }
-}
-
-async function notifConditioning(notifSwitch, timeOff, token) {
+async function notifConditioning(notifSwitch, timeOff, token, guiCond, espCond, serN) {
+  console.log(token)
   const messageRunning = {
     notification: {
       title: "Stove is Safety",
-      body: 'get close with your app'
+      body: `stove is on until timer is completed in ${timeOff}`
     },
     token: token,
   };
 
+  
+
   const messageNotRunning = {
     notification: {
       title: "Stove is not running",
-      body: 'manage your stove setup it can be more safety'
+      body: theMessageCond(guiCond, timeOff)
     },
     token: token,
   }
-  const messageTimeOff = {
-    notification: {
-      title: "Time Safety Completed",
-      body: `${toStringFromInt(timeOff)} is completed`
-    },
-    token: token
-  }
-  const messageTimeOn = {
-    notification: {
-      title: "Time Safety Start",
-      body: `${toStringFromInt(timeOff)} before your stove will turning off`
-    },
-    token: token
-  }
+  // const messageTimeOff = {
+  //   notification: {
+  //     title: "Time Safety Completed",
+  //     body: `${toStringFromInt(timeOff)} is completed`
+  //   },
+  //   token: token
+  // }
+  // const messageTimeOn = {
+  //   notification: {
+  //     title: "Time Safety Start",
+  //     body: `${toStringFromInt(timeOff)} before your stove will turning off`
+  //   },
+  //   token: token
+  // }
   switch (notifSwitch) {
     case 1: // notif for running stove
+    await db.collection('user').where('serialNumber', '==', serN).get().then((dz) => {
+      dz.forEach(cm => {
+        db.collection('user').doc(cm.id).update({
+          stoveStatus: espCond
+        })
+        db.collection('user').doc(cm.id).collection('stoveUsage').add({
+          time: admin.firestore.Timestamp.now(),
+          condition: espCond
+        })
+      })
+    })
       await notifAvailable(messageRunning)
       console.log(" notification 1")
       break
     case 2: // notif for off stove
+    await db.collection('user').where('serialNumber', '==', serN).get().then((dz) => {
+      dz.forEach(cm => {
+        db.collection('user').doc(cm.id).update({
+          stoveStatus: espCond
+        })
+        db.collection('user').doc(cm.id).collection('stoveUsage').add({
+          time: admin.firestore.Timestamp.now(),
+          condition: espCond
+        })
+      })
+    })
       await notifAvailable(messageNotRunning)
       console.log(" notification 2")
-      break
-    case 3: // notif for time event start conditon
-      await notifAvailable(messageTimeOn)
-      console.log("notification 3")
-      case 4:
-        await notifAvailable(messageTimeOff);
-        console.log("notification 4");
       break
     default:
       console.log("default")
@@ -140,10 +134,28 @@ async function notifConditioning(notifSwitch, timeOff, token) {
 
 
 // const ref = db.ref(`stove`)
-admin.database().ref('stove').on('value', (sn) => {
+admin.database().ref('stove').on('value', async (sn) => {
   sn.forEach(async (st) => {
     const data = st.val()
-    notifConditioning(data.notifCondition, data.timeOff, data.fcmToken)
+    if(!data.isRunningEsp){
+      admin.database().ref('stove').child(data.serialNumber).update({
+        isRunningGUI: false
+      })
+    }
+    // if(data.notifCondition == 1 || data.notifCondition == 2){
+    //   await db.collection('user').where('serialNumber', '==', data.serialNumber).get().then((dz) => {
+    //     dz.forEach(cm => {
+    //       db.collection('user').doc(cm.id).update({
+    //         stoveStatus: data.isRunningEsp
+    //       })
+    //       db.collection('user').doc(cm.id).collection('stoveUsage').add({
+    //         time: admin.firestore.Timestamp.now(),
+    //         condition: data.isRunningEsp
+    //       })
+    //     })
+    //   })
+    // }
+    notifConditioning(data.notifCondition, data.timeOff, data.fcmToken, data.isRunningGUI, data.isRunningEsp, data.serialNumber)
   })
 })
 
@@ -157,6 +169,14 @@ async function notifAvailable(theMsg) {
     .catch((error) => {
       console.log("Error sending message:", error);
     });
+}
+
+function theMessageCond (cond, time){
+  if(cond){
+    return "your stove is turning off"
+  }else{
+    return `your timer was completed in ${time}`;
+  }
 }
 
 
